599e907639a367c0b2fd7332bd51cfc6498f72c7
souloppen <arieloppenheimer@outlook.cl>
2025-08-29 16:09:14 -0400

    fix:order main

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from dotenv import load_dotenv
 from read_and_write import readTxt,writeTxt
 def main():
     load_dotenv()
-    invalid_domains=readTxt("./invalid.txt")
+    invalid_mail=readTxt("./invalid_mail.txt")
+    invalid_domains=readTxt("./invalid_domain.txt")
     if len(invalid_domains)>0:
-        writeTxt("./invalid.txt",invalid_domains,"Invalid Domains Found")
+        writeTxt("./invalid_domain.txt",invalid_domains,"Invalid Domains Found")
+    if len(invalid_mail)>0:
+        writeTxt("./invalid_mail.txt",invalid_domains,"Invalid Domains Found")
 if __name__=='__main__':
     main()
